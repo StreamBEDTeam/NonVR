62f9a33c5e3590fed3d70abf8072fd8468ea300a
Pranav Kulkarni <pranavkop@live.com>
2018-11-17 22:20:15 -0500

    Imported StreamBED.Frontend

--- StreamBED.Frontend/js/main.js
+++ StreamBED.Frontend/js/main.js
@@ -1,1 +1,8 @@
-﻿// Your code here!
+﻿function buttonOnMouseOver() {
+    document.getElementById("nextButton").style.backgroundColor = "#d4d4d4";
+    document.getElementById("nextButton").style.cursor = "pointer";
+}
+
+function buttonOnMouseLeave() {
+    document.getElementById("nextButton").style.backgroundColor = "#ececec";
+}
